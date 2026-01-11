8a416513d223a6bf671e41596ac1d04449bb633b
WalkQuackBack <quacksareback@duck.com>
2026-01-11 01:19:46 -0500

    feat: pseudo code for variants, general theme

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,13 +1,22 @@
+// Modified off of https://github.com/catppuccin/discord/blob/0fcfe2e15ed91f8c5f1f3c6855f3b2024f78d86c/build.js
+
 import * as sass from "sass";
+
+import type { CatppuccinFlavor, ColorFormat } from "@catppuccin/palette";
 
 const compileOptions: sass.Options<'sync'> = {
   loadPaths: ['node_modules'],
   quietDeps: true
 }
 
-function build() {
-  const compiled = sass.compile('./template/main.scss', compileOptions)
-  Deno.writeTextFile('./main.css', compiled.css)
+const baseTemplateString = Deno.readTextFileSync('./template/main.scss')
+
+function buildVariant(flavor: CatppuccinFlavor, accent: ColorFormat) {
+  const templateString = baseTemplateString
+  templateString.replace('');
+  const compiled = sass.compileString(baseTemplateString, compileOptions)
+  Deno.writeTextFile('./scss/', compiled.css)
+  Deno.writeTextFile('./themes/', compiled.css)
 }
 
-build()
+// build()
